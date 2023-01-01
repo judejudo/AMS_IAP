8bb9fd4c42a8e5203db488e2c44928c3185791cc
judejudo <86878327+judejudo@users.noreply.github.com>
2023-01-01 23:41:13 +0300

    factorydata

--- IAP_project/database/factories/StudentClassesFactory.php
+++ IAP_project/database/factories/StudentClassesFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\studentClasses>
  */
-class StudentClassesFactory extends Factory
+class studentClassesFactory extends Factory
 {
     /**
      * Define the model's default state.
